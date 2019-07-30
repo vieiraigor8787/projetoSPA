a2d24149a89bb71526462d739f8d2e831dd3898e
vieiraigor8787 <igor_vc@hotmail.com>
2019-07-30 14:32:59 -0300

    build das dependencias

--- gulp-tasks/deps.js
+++ gulp-tasks/deps.js
@@ -1,11 +1,17 @@
-const gulp =require('gulp')
+const gulp = require('gulp')
+const uglify = require('gulp-uglifycss')
+const concat = require('gulp-concat')
 
 gulp.task('deps', ['deps.css', 'deps.fonts'])
 
 gulp.task('deps.css', () => {
-
+    return gulp.src(['node_modules/font-awesome/css/font-awesome.css'])
+        .pipe(uglify({ "uglyComments": true }))
+        .pipe(concat('deps.min.css'))
+        .pipe(gulp.dest('build/assets/css'))
 })
 
 gulp.task('deps.fonts', () => {
-    
+    return gulp.src(['node_modules/font-awesome/fonts/*.*'])
+        .pipe(gulp.dest('build/assets/fonts'))
 })
